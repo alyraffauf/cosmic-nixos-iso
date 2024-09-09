1d8258e51287fb4b4aa6d5e9103b618fcabc4ab5
Aly Raffauf <aly@raffauflabs.com>
2024-09-09 10:27:03 -0400

    configuration: enable flakes in ISO

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@
   };
 
   nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
     substituters = ["https://cosmic.cachix.org/"];
     trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
   };
